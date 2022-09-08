9ddf43182a8e02734443249c783f50b1f8142618
SleepingDaemon <wiredgear@gmail.com>
2022-09-08 03:40:28 -0400

    Created a QuestPanel script to bind Quest UI elements

--- Assets/Scripts/Quests/Quest.cs
+++ Assets/Scripts/Quests/Quest.cs
@@ -6,19 +6,24 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Quest")]
 public class Quest : ScriptableObject
 {
-    [SerializeField] private string _name;
+    [SerializeField] private string _displayName;
     [SerializeField] private string _description;
 
     [Tooltip("Notes for ideas or obtaining more information")]
     [SerializeField] private string _notes;
 
     public List<Step> Steps;
+
+    public string Description => _description;
+
+    public string DisplayName => _displayName;
 }
 
 [Serializable]
 public class Step
 {
     [SerializeField] private string _instructions;
+    public string Instructions => _instructions;
     public List<Objective> Objectives;
 }
 
@@ -32,5 +37,10 @@ public class Objective
         Flag,
         Item,
         Kill,
+    }
+
+    public override string ToString()
+    {
+        return _objectiveType.ToString();
     }
 }
---
+++ Assets/Scripts/UI/QuestPanel.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuestPanel : MonoBehaviour
+{
+    [SerializeField] private Quest _selectedQuest;
+    [SerializeField] private Step _selectedStep;
+    [SerializeField] TMP_Text _nameText;
+    [SerializeField] TMP_Text _descriptionText;
+    [SerializeField] TMP_Text _currentObjectivesText;
+    [SerializeField] Image _iconImage;
+
+    [ContextMenu("Bind")]
+    public void Bind()
+    {
+        _nameText.SetText(_selectedQuest.DisplayName);
+        _descriptionText.SetText(_selectedQuest.Description);
+
+        _selectedStep = _selectedQuest.Steps.FirstOrDefault();
+
+        DisplayStepInstructionsAndObjectives();
+    }
+
+    private void DisplayStepInstructionsAndObjectives()
+    {
+        StringBuilder builder = new StringBuilder();
+        if (_selectedStep != null)
+        {
+            builder.AppendLine(_selectedStep.Instructions);
+            foreach (var objective in _selectedStep.Objectives)
+            {
+                builder.AppendLine(objective.ToString());
+            }
+        }
+
+        _currentObjectivesText.SetText(builder.ToString());
+    }
+}
