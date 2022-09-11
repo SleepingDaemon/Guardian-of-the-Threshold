a410c88a87d72d3260c787152c142c190318d90d
SleepingDaemon <wiredgear@gmail.com>
2022-09-11 07:47:51 -0400

    Improved performance of the FlagManager

--- Assets/Scripts/Flags/FlagManager.cs
+++ Assets/Scripts/Flags/FlagManager.cs
@@ -7,16 +7,15 @@ public class FlagManager : MonoBehaviour
     public static FlagManager Instance { get; private set; }
 
     [SerializeField] private List<GameFlagBase> _allFlags = new List<GameFlagBase>();
+    private Dictionary<string, GameFlagBase> _flagsByName;
 
-    private void Awake()
-    {
-        Instance = this;
-    }
+    private void Awake() => Instance = this;
+
+    private void Start() => _flagsByName = _allFlags.ToDictionary(k => k.name.Replace(" ", ""), v => v);
 
     public void Set(string flagName, string value)
     {
-        var flag = _allFlags.FirstOrDefault(t => t.name.Replace(" ", "") == flagName);
-        if(flag == null)
+        if(_flagsByName.TryGetValue(flagName, out var flag) == false)
         {
             Debug.LogError($"Flag not found {flagName}");
             return;
