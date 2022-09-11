6ba1798d9924491b81e24aac8cfeee317e1b7a7e
SleepingDaemon <wiredgear@gmail.com>
2022-09-11 08:40:43 -0400

    Added GetAllInstance() to FlagManager

--- Assets/Scripts/Flags/FlagManager.cs
+++ Assets/Scripts/Flags/FlagManager.cs
@@ -1,17 +1,35 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using UnityEditor;
 using UnityEngine;
 
 public class FlagManager : MonoBehaviour
 {
     public static FlagManager Instance { get; private set; }
 
-    [SerializeField] private List<GameFlagBase> _allFlags = new List<GameFlagBase>();
+    [SerializeField] private GameFlagBase[] _allFlags;
     private Dictionary<string, GameFlagBase> _flagsByName;
 
     private void Awake() => Instance = this;
 
     private void Start() => _flagsByName = _allFlags.ToDictionary(k => k.name.Replace(" ", ""), v => v);
+    private void OnValidate()
+    {
+        _allFlags = GetAllInstances<GameFlagBase>();
+    }
+
+    public static T[] GetAllInstances<T>() where T : ScriptableObject
+    {
+        string[] guids = AssetDatabase.FindAssets("t:" + typeof(T).Name);
+        T[] a = new T[guids.Length];
+        for (int i = 0; i < guids.Length; i++)
+        {
+            string path = AssetDatabase.GUIDToAssetPath(guids[i]);
+            a[i] = AssetDatabase.LoadAssetAtPath<T>(path);
+        }
+
+        return a;
+    }
 
     public void Set(string flagName, string value)
     {
