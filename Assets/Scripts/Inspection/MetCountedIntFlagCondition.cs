88d959e8d5786a546f4b0f9ebdec03c0fff4dd73
SleepingDaemon <wiredgear@gmail.com>
2022-09-18 06:53:02 -0400

    Added met counted int flag condition

---
+++ Assets/Scripts/Inspection/MetCountedIntFlagCondition.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class MetCountedIntFlagCondition : MonoBehaviour, IMet
+{
+    [SerializeField] private IntGameFlag _requiredFlag;
+    [SerializeField] private int _amountRequired = 4;
+
+    public bool Met()
+    {
+        return _requiredFlag.Value >= _amountRequired;
+    }
+}
