51415dcf0a00aa55bb6dd42b1079e4fe6a7a157c
SleepingDaemon <wiredgear@gmail.com>
2022-09-19 03:51:09 -0400

    Added ItemSlot class and refactored the Inventory class

--- Assets/Scripts/Inventory/Inventory.cs
+++ Assets/Scripts/Inventory/Inventory.cs
@@ -1,15 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
-    public List<Item> _item;
+    const int GENERAL_SIZE = 9;
+    public ItemSlot[] GeneralInventory = new ItemSlot[GENERAL_SIZE];
     public Item _debugItem;
+
+    private void Awake()
+    {
+        for (int i = 0; i < GENERAL_SIZE; i++)
+        {
+            GeneralInventory[i] = new ItemSlot();
+        }
+    }
 
     public void AddItem(Item item)
     {
-        _item.Add(item);
+        var firstEmptySlot = GeneralInventory.FirstOrDefault(t => t.IsEmpty);
+        firstEmptySlot.SetItem(item);
     }
 
     [ContextMenu(nameof(AddDebugItem))]
---
+++ Assets/Scripts/Inventory/ItemSlot.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+[Serializable]
+public class ItemSlot
+{
+    public Item _item;
+
+    public bool IsEmpty => _item == null;
+
+    public void SetItem(Item item)
+    {
+        _item = item;
+    }
+}
